6da76b1ba91efafda09461a3e51726683b4455f3
Pablok-jpg <60201737+Pablok-jpg@users.noreply.github.com>
2020-01-22 22:33:53 -0500

    Add files via upload
    Buenas noches profe, le anexo los programas 2 y 3 corregidos. Espero no sea muy tarde.
    Gracias.

---
+++ Ejercicio 3-arreglado.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos.
+            Console.WriteLine("Ingrese el valor del cateto Z:");
+            double z = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del ángulo C:");
+            double c = double.Parse(Console.ReadLine());
+            double cRad = c * Math.PI / 180;
+
+            //Cálculo del ángulo A
+            Console.WriteLine("Considerando el triángulo rectágulo, el valor del ángulo A es:");
+            double a = 180 - (90 + c);
+            Console.WriteLine(a);
+
+            //Cálculo de T
+            Console.WriteLine("El valor de la hipotenusa es:");
+            double t = z / (Math.Sin(cRad));
+            Console.WriteLine(t);
+
+            //Cálculo de Y.
+            Console.WriteLine("El valor del cateto Y es:");
+            double y = z / (Math.Tan(cRad));
+            Console.WriteLine(y);
+        }
+    }
+}
