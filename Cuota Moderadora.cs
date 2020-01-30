96bf92b199054bdad4e32728843e7e0b4ebbb93e
Pablok-jpg <60201737+Pablok-jpg@users.noreply.github.com>
2020-01-29 21:22:45 -0500

    Add files via upload

---
+++ Cuota Moderadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculemos su Cuota Moderadora:");
+            Console.WriteLine("Ingrese su salario:");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (salario < 1755606)
+            {
+                Console.WriteLine("Usted pertenece a la Tarifa A (3.400)");
+            }
+            else if (1755606 <= salario && salario <= 4389015)
+            {
+                Console.WriteLine("Usted pertenece a la Tarifa B (13.500)");
+            }
+            else
+            {
+                Console.WriteLine("Usted pertenece a la Tarifa C (35.600)");
+            }
+        }
+    }
+}
