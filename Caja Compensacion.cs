e0d2e49f03a79577f8482e2f52e2f2e7249913ba
Pablok-jpg <60201737+Pablok-jpg@users.noreply.github.com>
2020-01-29 21:15:57 -0500

    Add files via upload

---
+++ Caja Compensacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculemos su tarifa de Compensación Familiar:");
+            Console.WriteLine("Ingrese su salario:");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (salario < 1755606)
+            {
+                Console.WriteLine("Usted pertenece a la Tarifa A");
+            }
+             else if(1755606<=salario && salario <= 3511212)
+            {
+                Console.WriteLine("Usted pertenece a la Tarifa B");
+            }
+            else{
+                Console.WriteLine("Usted pertenece a la Tarifa C");
+            }
+        }
+    }
+}
